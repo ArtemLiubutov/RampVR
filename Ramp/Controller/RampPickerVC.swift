32a761ac4e87ffe9335564f344079d741eb3d967
artemLiubutov <lubutov033@gmail.com>
2017-10-30 22:22:11 +0300

    Add 3D assets and popup menu

---
+++ Ramp/Controller/RampPickerVC.swift
@@ -0,0 +1,45 @@
+//
+//  RampPickerVC.swift
+//  Ramp
+//
+//  Created by Artem Lubytov on 30.10.2017.
+//  Copyright © 2017 Artem Lubytov. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init (size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+   
+
+}
